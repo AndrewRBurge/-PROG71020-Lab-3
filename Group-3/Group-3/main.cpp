1bdcbd6147cd3faaeba4f7c53b8e12ea6829e473
andre <Aburge9773@conestogac.on.ca>
2025-06-10 17:22:10 -0400

    set up project
    -set up project with basic template
    -placed 2 classes to start off

---
+++ Group-3/Group-3/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+
+using namespace std;
+
+class rectangle {
+	int length;
+	int width;
+	int area;
+
+public: 
+
+		int GetLength() {
+		return(length);
+	}
+
+		void Setlength(int l) {
+			length = l;
+	}
+
+		int GetWidth() {
+			return(width);
+	}
+
+		void Setlength(int w) {
+			width = w;
+		}
+
+		int GetArea() {
+			return(area);
+		}
+		
+
+
+
+};
+
+class square {
+	int length;
+	int width;
+	int area;
+
+public:
+	int GetLength() {
+		return(length);
+	}
+	void Setlength(int l) {
+		length = l;
+	}
+
+	int GetWidth() {
+		return(width);
+	}
+
+	void Setlength(int w) {
+		width = w;
+	}
+
+	int GetArea() {
+		return(area);
+	}
+
+
+};
+
+
+int main(){
+
+
+
+
+}
